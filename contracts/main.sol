261aa4b715c18fc8f9e3edd9edc507b9ad62eb62
virtual <88248024+virtualjpeg@users.noreply.github.com>
2021-08-26 17:17:56 +0100

    Update main.sol

--- contracts/main.sol
+++ contracts/main.sol
@@ -1,1 +1,28 @@
+//SPDX-License-Identifier: MIT
 
+pragma solidity ^0.8.7;
+
+contract Ink {
+    
+    struct Post {
+        uint id;
+        address poster;
+        string content;
+        uint256 timePosted;
+    }
+    
+    mapping (uint => Post) posts;
+    uint postId;
+    
+    function createPost (string memory _content) public {
+        uint256 currentTime;
+        currentTime == block.timestamp;
+        posts[postId] = Post(postId, msg.sender, _content, currentTime);
+        postId++;
+    }
+    
+    function postAmount() public view returns (uint) {
+        return postId;
+    }
+    
+}
