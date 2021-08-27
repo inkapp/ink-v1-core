1756276441b2c43600d12adda4c2496f8be03647
virtual <88248024+virtualjpeg@users.noreply.github.com>
2021-08-27 13:29:48 +0100

    Update invite.sol

--- contracts/invite.sol
+++ contracts/invite.sol
@@ -1,1 +1,15 @@
+//SPDX-License-Identifier: MIT
 
+pragma solidity ^0.8.1;
+
+contract invite {
+    
+    function claimToken() {
+        
+    }
+    
+    function inviteUser() {
+        
+    }
+    
+}
