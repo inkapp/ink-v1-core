fc36bfb406fced907d4aae3ea69e08942ffa3432
virtual <88248024+virtualjpeg@users.noreply.github.com>
2021-08-28 13:42:19 +0100

    Update invite.sol

--- contracts/invite.sol
+++ contracts/invite.sol
@@ -6,6 +6,7 @@ contract invite {
     
     //can't claim tokens more than once per address
     mapping (address => bool) public claimed;
+    mapping (address => bool) public invited;
     
     function claimToken() public {
         //send token to user
@@ -13,8 +14,10 @@ contract invite {
         claimed[msg.sender] = true;
     }
     
-    function inviteUser() public {
-        
+    function inviteUser(address _invite1, address _invite2) public {
+        require(ink.balanceOf(user) > 0)
+        invited[_invite1] = true;
+        invited[_invite2] = true;
     }
     
 }
